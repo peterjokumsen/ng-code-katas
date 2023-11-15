849b3b21f32eb1f6de02dc1d0519a4de88685e84
Peter Jokumsen <pjokumsen@gmail.com>
2023-11-15 17:54:05 +0200

    feat(kata-2): create basic calculator

--- kata-2-calc/src/app/app.component.ts
+++ kata-2-calc/src/app/app.component.ts
@@ -1,14 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { catchError, map, startWith } from 'rxjs/operators';
+
+interface CalcOperator {
+  key: string;
+  label: string;
+  operation: (a: number, b: number) => number;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, ReactiveFormsModule, RouterOutlet]
 })
-export class AppComponent {
-  title = 'kata-2-calc';
+export class AppComponent implements AfterViewInit {
+  private formBuilder = inject(FormBuilder);
+
+  readonly operators: CalcOperator[] = [
+    { key: 'plus', label: 'Add', operation: (a, b) => a + b },
+    { key: 'minus', label: 'Subtract', operation: (a, b) => a - b },
+    { key: 'multiply', label: 'Multiply', operation: (a, b) => a * b },
+    { key: 'divide', label: 'Divide', operation: (a, b) => a / b },
+    { key: 'mod', label: 'Modulus', operation: (a, b) => a % b },
+  ];
+
+  @ViewChildren('numberInput') numberInputs: QueryList<ElementRef> | undefined;
+
+  calcFormGroup = this.formBuilder.group({
+    firstNumber: ['', [Validators.required]],
+    secondNumber: ['0', [Validators.required]],
+    operator: 'plus',
+  });
+
+  result$ = this.calcFormGroup.valueChanges.pipe(
+    map(({ firstNumber, secondNumber, operator }) => {
+      const first = parseFloat(firstNumber ?? '0');
+      const second = parseFloat(secondNumber ?? '0');
+      const selectedOperator = this.operators.find(op => op.key === operator);
+      return selectedOperator?.operation(first, second) ?? 0;
+    }),
+    map((result) => (isNaN(result) ? '🤖 could not compute' : `${result}`)),
+    catchError(() => ['Something went wrong 🤷‍♂️']),
+  );
+
+  ngAfterViewInit(): void {
+    this.numberInputs?.first?.nativeElement?.focus();
+  }
 }
